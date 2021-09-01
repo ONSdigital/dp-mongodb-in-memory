85e3111e23410346e61d951ff0652a71b1a94cfe
Rafa <rafael.garcia-pendon@ons.gov.uk>
2021-09-01 16:14:59 +0100

    Add test and assertions

--- main_test.go
+++ main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"strconv"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -16,9 +19,36 @@ func TestStart(t *testing.T) {
 		Convey("Given the version "+version, t, func() {
 			Convey("When the Start method is called", func() {
 				server, err := Start(version)
+				defer server.Stop()
+
 				Convey("Then no error is returned", func() {
 					So(err, ShouldBeNil)
+				})
+				Convey("And the mongod process has run", func() {
 					So(server, ShouldNotBeNil)
+					So(server.cmd, ShouldNotBeNil)
+					So(server.dbDir, ShouldNotBeBlank)
+					So(server.port, ShouldNotBeEmpty)
+					So(server.cmd.Args[0], ShouldEndWith, "mongod")
+					So(server.cmd.Args[1], ShouldEqual, "--storageEngine")
+					So(server.cmd.Args[2], ShouldEqual, "ephemeralForTest")
+					So(server.cmd.Args[3], ShouldEqual, "--dbpath")
+					So(server.cmd.Args[4], ShouldEqual, server.dbDir)
+					So(server.cmd.Args[5], ShouldEqual, "--port")
+					So(server.cmd.Args[6], ShouldEqual, strconv.Itoa(server.port))
+				})
+				Convey("And the watcher process has run", func() {
+					expectedScript := fmt.Sprintf("while ps -o pid= -p %d; "+
+						"do sleep 1; "+
+						"done; "+
+						"kill -9 %d",
+						os.Getpid(), server.cmd.Process.Pid)
+
+					So(server, ShouldNotBeNil)
+					So(server.watcherCmd, ShouldNotBeNil)
+					So(server.watcherCmd.Args[0], ShouldEndWith, "/bin/sh")
+					So(server.watcherCmd.Args[1], ShouldEqual, "-c")
+					So(server.watcherCmd.Args[2], ShouldEqual, expectedScript)
 				})
 				Convey("And the server accepts connections", func() {
 					client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(server.URI()))
